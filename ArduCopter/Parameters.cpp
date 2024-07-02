46acb5f223cb5d40971e57b61007a13af812745b
Ryoichi <contact@hamalt.studio>
2024-07-02 10:05:12 +0900

    オートフォーマットの差分をなくすテスト2

--- ArduCopter/Parameters.cpp
+++ ArduCopter/Parameters.cpp
@@ -41,7 +41,7 @@ const AP_Param::Info Copter::var_info[] = {
     // @Description: Allows setting an individual MAVLink system id for this vehicle to distinguish it from others on the same network
     // @Range: 1 255
     // @User: Advanced
-    GSCALAR(sysid_this_mav, "SYSID_THISMAV", MAV_SYSTEM_ID),
+    GSCALAR(sysid_this_mav, "SYSID_THISMAV",   MAV_SYSTEM_ID),
 
     // @Param: SYSID_MYGCS
     // @DisplayName: My ground station number
@@ -49,7 +49,7 @@ const AP_Param::Info Copter::var_info[] = {
     // @Range: 1 255
     // @Increment: 1
     // @User: Advanced
-    GSCALAR(sysid_my_gcs, "SYSID_MYGCS", 255),
+    GSCALAR(sysid_my_gcs,   "SYSID_MYGCS",     255),
 
     // @Param: PILOT_THR_FILT
     // @DisplayName: Throttle filter cutoff
